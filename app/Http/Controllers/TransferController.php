8c23d2d7e8f935924461016c4dffa95b713626f1
KANATA1 <fehrfaber@gmail.com>
2019-04-29 03:55:16 -0600

    transfer tab

--- app/Http/Controllers/TransferController.php
+++ app/Http/Controllers/TransferController.php
@@ -9,7 +9,14 @@ class TransferController extends Controller
 {
     public function index(Request $request)
     {
-        return view('backEnd.transfer.index');
+        $data = DB::select('select * from lumberyardbranch');
+
+        $branches= [];
+        foreach ($data as $branch) {
+            $branches[] = array('id' => $branch->id,'name'=>$branch->district);
+        }
+
+        return view('backEnd.transfer.index',$branches);
     }
 
     public function store(Request $request)
--- resources/views/backEnd/transfer/index.blade.php
+++ resources/views/backEnd/transfer/index.blade.php
@@ -20,7 +20,7 @@ Transfer
 
                 <div>@include('backEnd.qr.scanqrcode')</div> <!-- This Line Creates the Scan QR Button anywhere you put this piece of code -->
                 QR Code Value:
-                <input type="text" class="input-group" id="qrCode"name="qrCode" style="display: inline-block;" disabled> <!-- This is required cause the result will be added to this. Could be put any where -->
+                <input type="text" class="input-group-text" id="qrCode"name="qrCode" style="display: inline-block;" disabled> <!-- This is required cause the result will be added to this. Could be put any where -->
                 <!-------------------------->
 
                 </div>
@@ -36,6 +36,7 @@ Transfer
             <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
                 {!! Form::label('transferto', 'Transfer to:', ['class' => 'col-sm-3 control-label']) !!}
                 <div class="col-sm-6">
+                    
                     {!! Form::select('transfer',['Belize Branch'],null,['class' => 'form-control']) !!}
                 </div>
             </div> 
